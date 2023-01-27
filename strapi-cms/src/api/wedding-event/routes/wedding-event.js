8070703772a17ca6ee829fbc95ff7998e47d465b
Austin Shih <austinhshih@gmail.com>
2023-01-26 19:12:34 -0800

    Added strapi
    Created wedding-events

---
+++ strapi-cms/src/api/wedding-event/routes/wedding-event.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * wedding-event router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::wedding-event.wedding-event');
