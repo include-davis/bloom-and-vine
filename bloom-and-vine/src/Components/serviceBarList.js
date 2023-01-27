93ba3b36a6e2dc9951f0a3324bbc8a9dc923904e
Sauvikesh <81947858+Sauvikesh@users.noreply.github.com>
2023-01-26 17:52:37 -0800

    Finished the services bar

--- bloom-and-vine/src/Components/serviceBarList.js
+++ bloom-and-vine/src/Components/serviceBarList.js
@@ -1,11 +1,25 @@
 import "./serviceBar.css"
 
 import ServiceBarElm from "./serviceBarElements";
+
 import wedding from "../Pages/serviceImages/weddings.png"
+import rentals from "../Pages/serviceImages/rentals.png"
+import funerals from "../Pages/serviceImages/funerals.png"
+import classes from "../Pages/serviceImages/classes.png"
+import parties from "../Pages/serviceImages/parties.png"
+import customOrder from "../Pages/serviceImages/customOrder.png"
 
 const ServiceBarList = () =>{
     return(
-        <ServiceBarElm img={wedding} name ="Weddings"/>
+        <div id = "servBar">
+            <ServiceBarElm img={wedding} name ="Weddings"/>
+            <ServiceBarElm img={rentals} name ="Rentals"/>
+            <ServiceBarElm img={funerals} name ="Funerals"/>
+            <ServiceBarElm img={classes} name ="Classes"/>
+            <ServiceBarElm img={parties} name ="Parties"/>
+            <ServiceBarElm img={customOrder} name ="Custom Orders"/>
+        </div>
+        
     )
 };
 
