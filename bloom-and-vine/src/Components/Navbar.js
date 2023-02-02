bb58cf582bb6248c5fd2d8038f71e29f7af8bf6f
Matt <98845506+mutthias@users.noreply.github.com>
2023-02-01 22:39:23 -0800

    basic navbar
    basic and unpolished version of the navbar with the logo, as well as other page directory elements

--- bloom-and-vine/src/Components/Navbar.js
+++ bloom-and-vine/src/Components/Navbar.js
@@ -3,16 +3,30 @@ import './Navbar.css';
 
 function Navbar() {
   return (
-    <div className="Navbar">
+    <nav className="Navbar">
       <div className='Logo'>
-        <a href='/' classname='logo-press'>
+        <a href='/home' className='main-logo'>
           Bloom & Vine
         </a>
       </div>
-      <li className='Links'>
-        
-      </li>
-    </div>
+      <ul className='directory'>
+        <li>
+          <a href='/home'>Home</a>
+        </li>
+        <li>
+          <a href='/gallery'>Gallery</a>
+        </li>
+        <li>
+          <a href='/services'>Services</a>
+          </li>
+        <li>
+          <a href='/about'>About</a>
+        </li>
+        <li>
+          <a href='/contact'>Contact</a>
+        </li>
+      </ul>
+    </nav>
   );
 }
 
