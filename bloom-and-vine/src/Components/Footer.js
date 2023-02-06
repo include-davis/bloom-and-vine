4505d10d6bf4105f8525dd419010ededed79b2ca
mutthias <98845506+mutthias@users.noreply.github.com>
2023-02-06 11:42:27 -0800

    Cleanup
    cleaning up unnecessary code (also removing a comment my friend typed that i believe accidentally got committed)

--- bloom-and-vine/src/Components/Footer.js
+++ bloom-and-vine/src/Components/Footer.js
@@ -33,8 +33,16 @@ function Footer() {
             <a href='/contact'>Contact</a>
           </li>
         </ul>
+
+        <div className='Instagram'> 
+          <svg width="13" height="22" viewBox="0 0 13 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M12 1H9C7.67392 1 6.40215 1.52678 5.46447 2.46447C4.52678 3.40215 4 4.67392 4 
+            6V9H1V13H4V21H8V13H11L12 9H8V6C8 5.73478 8.10536 5.48043 8.29289 5.29289C8.48043 5.10536 8.73478 5 9 5H12V1Z" 
+            stroke="blue" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+          </svg>
+        </div>
+
         
-        <div className='images'>
         <div className='Pinterest'>
           <img src="https://cdn.discordapp.com/attachments/767939191962599478/1072037199161335918/image.png"></img>
         </div>
@@ -49,7 +57,7 @@ function Footer() {
         </div>
         </div>
         <div class="push"></div>
-      </div>
+      
       <footer class="footer"></footer>
     </div>
     
