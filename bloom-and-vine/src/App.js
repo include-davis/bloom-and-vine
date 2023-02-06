faf45db832f5fbb9afbc8cd70d60b7f1b6f0ba2e
mutthias <98845506+mutthias@users.noreply.github.com>
2023-02-05 20:14:26 -0800

    footer testing
    creating the footer, working on sticky footer

--- bloom-and-vine/src/App.js
+++ bloom-and-vine/src/App.js
@@ -2,11 +2,31 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Navbar from './Components/Navbar';
+import Footer from './Components/Footer';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+    
+      <p>test</p>
+      <p>test</p>
+      <p>test</p>
+      <p>test</p>
+
+      <p>test</p><p>test</p>
+      <p>test</p>
+
+      <p>test</p><p>test</p>
+      <p>test</p>
+
+      <p>test</p>
+      <p>test</p>
+      <p>test</p>
+      
+      
+
+      <Footer/>
     </div>
   );
 }
--- bloom-and-vine/src/Components/Footer.js
+++ bloom-and-vine/src/Components/Footer.js
@@ -3,8 +3,41 @@ import './Footer.css';
 function Footer() {
   return (
     <div className="Footer">
+      <div className='wrapper'>
+
       
+        <div className='Logo'>
+          Bloom & Vine
+        </div>
+
+        <p className='Description'>
+          Boutique floral design studio serving
+          <b> Northern California.</b> 
+          We work to make every event unforgettable. 
+        </p>
+
+        <ul className='directory'>
+          <li>
+            <a href='/home'>Home</a>
+          </li>
+          <li>
+            <a href='/gallery'>Gallery</a>
+          </li>
+          <li>
+            <a href='/services'>Services</a>
+            </li>
+          <li>
+            <a href='/about'>About</a>
+          </li>
+          <li>
+            <a href='/contact'>Contact</a>
+          </li>
+        </ul>
+        <div class="push"></div>
+      </div>
+      <footer class="footer"></footer>
     </div>
+    
   );
 }
 
