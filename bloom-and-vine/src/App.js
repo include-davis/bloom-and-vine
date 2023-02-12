734cc5b5d57398176e077d32f5b34e6d6f032736
Matt <98845506+mutthias@users.noreply.github.com>
2023-02-12 14:49:42 -0800

    contact form left side
    first unfinished commit of the contact page, this just covers the left-hand side of the contact page, minus the socials on the bottom which will be added soon. CSS needs to be edited too.

--- bloom-and-vine/src/App.js
+++ bloom-and-vine/src/App.js
@@ -1,13 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Navbar from './Components/Navbar';
 import Footer from './Components/Footer';
+import Contact from './Pages/Contact';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+
+      <Contact />
+
+      {/* <p>test</p>
+      <p>test</p>
+      <p>test</p>
+      <p>test</p>
+      <p>test</p>
+      <p>test</p>
+      <p>test</p>
+      <p>test</p>
+      <p>test</p>
+      <p>test</p>
+      <p>test</p>
+      <p>test</p>
+      <p>test</p>
+      <p>test</p>
+      <p>test</p>
+      <p>test</p>
+      <p>test</p>
+      <p>test</p>
+      <p>test</p>
+      <p>test</p>
+      <p>test</p>
+      <p>test</p>
+      <p>test</p>
+      <p>test</p>
+      <p>test</p> */}
 
       <Footer/>
     </div>
