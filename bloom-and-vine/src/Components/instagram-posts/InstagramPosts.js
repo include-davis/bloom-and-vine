e3caf70310663a98d398e5b7b6c068e84c93768c
Ryan <ryyu444@gmail.com>
2023-02-14 01:25:38 -0800

    Added Instagram Shoutout display & Instagram Posts skeleton

---
+++ bloom-and-vine/src/Components/instagram-posts/InstagramPosts.js
@@ -0,0 +1,9 @@
+import './InstagramPosts.css'
+
+export default function InstagramPosts () {
+    return (
+        <div>
+            <h1>POSTS</h1>
+        </div>
+    )
+};
---
+++ bloom-and-vine/src/Components/instagram-shoutout/InstagramShoutout.js
@@ -0,0 +1,16 @@
+import './InstagramShoutout.css'
+
+export default function InstagramSlider () {
+    const InstagramHandler = (e) => {
+        e.preventDefault()
+    
+        location.href = 'https://instagram.com'
+    }
+
+    return (
+        <div>
+            <p className='instagram-shoutout'>Check us out on Instagram!</p>
+            <button className='instagram-tag' onClick={InstagramHandler}>@bloomandvine</button>
+        </div>
+    )
+}
