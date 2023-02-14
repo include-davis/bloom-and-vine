7d670267b37aed4b24e9cd722c4fce63f4c559b0
Ryan <ryyu444@gmail.com>
2023-02-14 01:27:07 -0800

    Modified Homepage to fit with components

--- bloom-and-vine/src/Pages/Homepage.js
+++ bloom-and-vine/src/Pages/Homepage.js
@@ -1,19 +1,12 @@
-import { useState } from 'react';
-
 import Navbar from '../Components/Navbar'
-import UserReview from '../Components/Review';
+import ReviewSlider from '../Components/review-slider/ReviewSlider';
+import InstagramShoutout from '../Components/instagram-shoutout/InstagramShoutout';
+import InstagramPosts from '../Components/instagram-posts/InstagramPosts';
 import displayImg from './images/DISPLAY-60.jpg'
 import './Homepage.css';
 
 function Homepage() {
   // Create custom hook to return the current review displayed
-  // const [review, setReview] = useState()
-  const userReviews = [
-    {
-      'user': "tanweddingsandevents",
-      'comment': "Such an exquisite day! Flowers were perfection."
-    },
-  ]
 
   return (
     <div className="Homepage">
@@ -28,18 +21,16 @@ function Homepage() {
       </div>
 
       <div className='review-display'>
-        {userReviews.map(review => 
-          <UserReview review={review}/>
-        )}
+        <ReviewSlider />
       </div>
 
       <div className='redirect-display'>
-        <p>world</p>
+        <h1>Redirect Images</h1>
       </div>
 
       <div className='instagram-display'>
-        <p className='instagram-shoutout'>Check us out on Instagram!</p>
-        <a className='instagram-tag' href='https://instagram.com'>@bloomandvine</a>
+        <InstagramShoutout />
+        <InstagramPosts />
       </div>
 
     </div>
