6ef57ca8df53090fd93e5f6c071114705d7e88da
Matt <98845506+mutthias@users.noreply.github.com>
2023-02-17 16:34:38 -0800

    Routes Setup
    Created Routing in App.js. Routes can be set up for respective pages. For example, click on 'contact' on the navbar/footer and it will appear.

--- bloom-and-vine/src/App.js
+++ bloom-and-vine/src/App.js
@@ -1,20 +1,24 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 import { FaHeart } from "react-icons/fa";
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom"
 import Navbar from './Components/Navbar';
 import Footer from './Components/Footer';
 import Contact from './Pages/Contact';
 
 function App() {
   return (
-    <div className="App">
+    
+      <Router>
       <Navbar />
-
-      <Contact />
+      <Routes>
+        <Route exact path='/Contact' element={< Contact />}></Route>
+      </Routes>
 
       <Footer/>
-    </div>
+
+      </Router>
+    
   );
 }
 
