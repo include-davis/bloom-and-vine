7cb08b329f3fec5a74a151a1b894d11c501c4cc2
Matt <98845506+mutthias@users.noreply.github.com>
2023-02-23 19:27:07 -0800

    font fix

--- bloom-and-vine/src/App.js
+++ bloom-and-vine/src/App.js
@@ -1,6 +1,5 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import './App.css';
-import { FaHeart } from "react-icons/fa";
 import { BrowserRouter as Router, Routes, Route} from "react-router-dom"
 import Navbar from './Components/Navbar';
 import Footer from './Components/Footer';
