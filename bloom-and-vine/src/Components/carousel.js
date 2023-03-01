db02d1bb75c14e266c304bd872379f520a533c4f
Sauvikesh <81947858+Sauvikesh@users.noreply.github.com>
2023-02-28 17:53:28 -0800

    fixed most of the responsiveness.
    The contact us button gets weird under 1300 px width.

--- bloom-and-vine/src/Components/carousel.js
+++ bloom-and-vine/src/Components/carousel.js
@@ -73,7 +73,6 @@ const ServicesCarousel = () =>{
                 </div>
                 
             </div>
-
         </div>
         
     )
--- bloom-and-vine/src/Components/serviceBarElements.js
+++ bloom-and-vine/src/Components/serviceBarElements.js
@@ -1,12 +1,26 @@
 import "./serviceBar.css"
 
 function ServiceBarElm(props){
-    return(
-        <div id = "barElm">
-            <img className= "servImg" src={props.img} alt={props.name} />
-            <h3 className= "servHeader" >{props.name}</h3>
-        </div>
-    )
+
+    if(props.name == "Custom Orders"){
+        return(
+            <div id = "barElm">
+                <img className= "servImg" src={props.img} alt={props.name} />
+    
+                <h3 className= "servHeader2" >{props.name}</h3>
+            </div>
+        )
+    }
+    else{
+        return(
+            <div id = "barElm">
+                <img className= "servImg" src={props.img} alt={props.name} />
+    
+                <h3 className= "servHeader" >{props.name}</h3>
+            </div>
+        )
+    }
+
 };
 
 export default ServiceBarElm;
