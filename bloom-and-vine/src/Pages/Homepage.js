87a3510239ff9763eea9128a15411d1d16ae62d3
Ryan <ryyu444@gmail.com>
2023-03-08 18:49:49 -0800

    Added responsiveness to images, sliders, & text

--- bloom-and-vine/src/Pages/Homepage.js
+++ bloom-and-vine/src/Pages/Homepage.js
@@ -4,8 +4,12 @@ import ReviewSlider from '../Components/review-slider/ReviewSlider';
 import RedirectImages from '../Components/redirect-images/RedirectDisplay';
 import InstagramShoutout from '../Components/instagram-shoutout/InstagramShoutout';
 import InstagramPostsSlider from '../Components/instagram-posts/InstagramPostsSlider';
-import displayImg from '../Images/DISPLAY-60.jpg'
+import displayImg from '../Images/DISPLAY-60_cropped.jpg'
 import './Homepage.css';
+
+// Put equal spacing between img, reviews, & instagram shoutout (DONE)
+
+// Indication that redirect has been clicked (darkened image)
 
 function Homepage() {
 
