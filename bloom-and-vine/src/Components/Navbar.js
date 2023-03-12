59ad0a337a76d6e563f0e8ec1ef3edf6301c92d7
Ryan <ryyu444@gmail.com>
2023-03-11 18:40:01 -0800

    Removed Navbar, Footer, & Services Components

--- bloom-and-vine/src/Components/Navbar.js
+++ bloom-and-vine/src/Components/Navbar.js
@@ -1,33 +1,10 @@
 import './Navbar.css';
-import React from 'react'
-
 
 function Navbar() {
   return (
-    <nav className="Navbar">
-      <div className='Logo'>
-        <a href='/home' className='main-logo'>
-          Bloom & Vine
-        </a>
-      </div>
-      <ul className='directory'>
-        <li>
-          <a href='/'>Home</a>
-        </li>
-        <li>
-          <a href='/gallery'>Gallery</a>
-        </li>
-        <li>
-          <a href='/services'>Services</a>
-          </li>
-        <li>
-          <a href='/about'>About</a>
-        </li>
-        <li>
-          <a href='/contact'>Contact</a>
-        </li>
-      </ul>
-    </nav>
+    <div className="Navbar">
+      
+    </div>
   );
 }
 
