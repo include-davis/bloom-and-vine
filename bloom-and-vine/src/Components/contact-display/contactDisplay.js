77b6f31993798e2710bbda113683e2260a26d28d
Sauvikesh <81947858+Sauvikesh@users.noreply.github.com>
2023-03-14 19:37:05 -0700

    Finished the contactDisplay Component
    Also added an image of the arrow to the images folder

--- bloom-and-vine/src/Components/contact-display/contactDisplay.js
+++ bloom-and-vine/src/Components/contact-display/contactDisplay.js
@@ -1,9 +1,26 @@
 import './contactDisplay.css'
 
+import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom'
+import arrow from "../../Images/upArrow.png"
+
+function scrollToTop(){
+    window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: 'smooth'
+      });
+}
+
 export default function ContactDisplay () {
     return (
 	    <div>
-			testing again!
+			<p id = "bottomText">Interested in booking for an event?</p>
+            <div id = "bottomButtons">
+                <NavLink id = "contactButtonM"to="/Contact">
+                    <div >Contact Us</div>
+                </NavLink>
+                <img onClick= {scrollToTop} id = "arrowButton" src={arrow}  />
+            </div>
         </div>
     );
 }
