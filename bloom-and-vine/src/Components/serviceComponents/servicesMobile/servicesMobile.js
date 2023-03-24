3a9530965ba56e80759325ef911fd0b1e227b4cf
Sauvikesh <81947858+Sauvikesh@users.noreply.github.com>
2023-03-24 00:37:24 -0700

    Added descriptions provided by designer

--- bloom-and-vine/src/Components/serviceComponents/servicesMobile/servicesMobile.js
+++ bloom-and-vine/src/Components/serviceComponents/servicesMobile/servicesMobile.js
@@ -24,27 +24,27 @@ function ServicesMobile() {
         {
           service: "Rentals",
           src: rentals,
-          description: "2nd img words words words words words words words words words words words words words words words words words words words words words words words words words words words words "
+          description: "Interested in renting homemade decor pieces for a special event? Contact us for more information about our available rentals and we’ll reach back to you shortly! Let us help you find the piece that will make your event shine even brighter!"
         },
         {
           service: "Funerals",
           src: funerals,
-          description: "3rd img words words words words words words words words words words words words words words words words words words words words words words "
+          description: "Need assistance with floral arrangements for a funeral? Get in touch so we can help you find the best flowers to honor your passed loved one."
         },
         {
           service: "Classes",
           src: classes,
-          description: "4th img words words words words words words words words words words words words words words words words words words words words words words "
+          description: " Interested in learning how to DIY your own decor pieces? We offer a variety of classes that range from DIYing centerpieces to arranging flowers like a professional! No matter your experience, we’ll help you every step of the way toward achieving your creative goals. "
         },
         {
           service: "Parties",
           src: parties,
-          description: "5th img words words words words words words words words words words words words words words words words words words words words words words "
+          description: "Looking to plan the party of your dreams? Chat with us and we’ll gladly help you bring your party plans to life! Whether you need help with organizing a party you already had in mind or with planning a party from step one, we’ll make sure you are 100% satisfied! "
         },
         {
           service: "Custom Orders",
           src: customOrder,
-          description: "6th img words words words words words words words words words words words words words words words words words words words words words words "
+          description: "Need help with planning something that goes beyond our other services? We are always open to making custom orders to help you bring your dreams to reality! Contact us and let us know about your vision so we can best help you achieve it! "
         }
       ];
 
