215a7eb70015887f8ba94b0089f02febfd2e13e6
Ryan <ryyu444@gmail.com>
2023-03-27 20:02:33 -0700

    Revert "Added more autoscroll for ReviewSlider and display responsiveness"
    This reverts commit 7c908084d6d0b24500b7886519caf15e4edcff32.

--- bloom-and-vine/src/Components/review-slider/ReviewSlider.js
+++ bloom-and-vine/src/Components/review-slider/ReviewSlider.js
@@ -27,78 +27,65 @@ const userReviews = [
     },
   ].map((userReview) => <UserReview review={userReview} />)
 
-function getReviewDots(total, setCurReviewDot) {
+function getReviewDots(total) {
     let dot_display = []
     for (let i = 0; i < total; i++) {
         dot_display.push(
-            <div className='review-dot' key={i} onClick={() => setCurReviewDot(i)}></div>
-        );
+            <div className='review-dot' key={i}></div>
+        )
     }
 
-    return dot_display;
+    return dot_display
 }
 
 function setCurrentDot(index) {
     const curIndex = index + 1 <= userReviews.length ? index + 1 : 1;
     const inactiveDots = document.querySelectorAll(`.review-dot:not(:nth-child(${curIndex}))`)
     const curDot = document.querySelector(`.review-dot:nth-child(${curIndex})`)
-    
+
     for (let i = 0; i < inactiveDots.length; i++) {
         inactiveDots[i].style.backgroundColor = "#D9D9D9";
     }
-    
+
     curDot.style.backgroundColor = "#A28598";
 }
 
-// Scroller Settings: Interval (Delay for how long each scroll is in ms)
-const autoScroll = false;
-const autoScrollInterval = 3500;
+export default function ReviewBar() {
+    const [curReview, setCurReview] = useState(userReviews[0])
+    const [index, setIndex] = useState(0)
+    
+    const reviewDots = getReviewDots(userReviews.length)
+        
+    useEffect(() => {
+        setCurrentDot(index)
+        setCurReview(userReviews[index])
+    }, [index])
+        
 
-export default function ReviewSlider() {
-    const [curReviewIndex, setCurReviewIndex] = useState(0);
-    const reviewDots = getReviewDots(userReviews.length, setCurReviewIndex);
-
-    // Logic: Click left or right --> Increment/Decrement curReviewIndex --> Display different review
-    const reviewButtonHandler = (e, button) => {
+    // Logic: Click left or right --> Increment/Decrement Index --> Display different review
+    const onClickHandler = (e, button) => {
         e.preventDefault();
     
         if (button === 'forward') {
-            curReviewIndex < userReviews.length - 1 ? setCurReviewIndex(prevIndex => prevIndex + 1) : setCurReviewIndex(0)
+            index < userReviews.length - 1 ? setIndex(index => index + 1) : setIndex(0)
         }
         else if (button === 'back') {
-            curReviewIndex > 0 ? setCurReviewIndex(prevIndex => prevIndex - 1) : setCurReviewIndex(userReviews.length - 1)
+            index > 0 ? setIndex(index => index - 1) : setIndex(userReviews.length - 1)
         }
     }
 
-    // AUTOSCROLLER - Will not be paused by button presses
-    const autoScrollRight = () => {
-        setCurReviewIndex((prevIndex) => (prevIndex + 1 < userReviews.length ? prevIndex + 1 : 0));
-    };
-
-    useEffect(() => {
-        if (!autoScroll) {
-            return;
-        }
-        const slideInterval = setInterval(autoScrollRight, autoScrollInterval);
-        return () => clearInterval(slideInterval);
-    }, []);
-
-    useEffect(() => {
-        setCurrentDot(curReviewIndex);
-    }, [curReviewIndex]);
-
     return (
         <div className='review-slider'>
-            <button className='backward-review-button' onClick={e => reviewButtonHandler(e, 'back')}>
+            <button className='backward-review-button' onClick={e => onClickHandler(e, 'back')}>
                 <img className='back-button' src={BackArrow} />
             </button>
             <div className='current-review-display'>
-                {userReviews[curReviewIndex]}
-                <div className='review-dot-container'>
+                {curReview}
+                <div className='review-dots'>
                     {reviewDots}
                 </div>
             </div>
-            <button className='forward-review-button' onClick={e => reviewButtonHandler(e, 'forward')}>
+            <button className='forward-review-button' onClick={e => onClickHandler(e, 'forward')}>
                 <img className='forward-button' src={ForwardArrow} />
             </button>
         </div>
