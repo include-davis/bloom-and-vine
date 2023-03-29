f015b0f5c0978f4965a9891eba44fd0f1e349e4d
Ryan <ryyu444@gmail.com>
2023-03-29 14:59:11 -0700

    Swapped shoutout button to a tag

--- bloom-and-vine/src/Components/instagram-shoutout/InstagramShoutout.js
+++ bloom-and-vine/src/Components/instagram-shoutout/InstagramShoutout.js
@@ -1,16 +1,10 @@
 import './InstagramShoutout.css'
 
 export default function InstagramShoutout () {
-    const InstagramHandler = (e) => {
-        e.preventDefault()
-        
-        location.href = `https://www.instagram.com/bloomandvine/`
-    }
-
     return (
         <div className='instagram-shoutout-container'>
             <p className='instagram-shoutout'>Check us out on Instagram!</p>
-            <button className='instagram-tag' onClick={InstagramHandler}>@bloomandvine</button>
+            <a className='instagram-tag' href='https://www.instagram.com/bloomandvine/'>@bloomandvine</a>
         </div>
     )
 }
