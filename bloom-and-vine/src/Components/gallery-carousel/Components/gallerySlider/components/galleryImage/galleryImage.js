ca300c95e77b947a3c79fe9a0afd6d4459829d89
Ryan <ryyu444@gmail.com>
2023-03-31 02:32:50 -0700

    Restored navigation by clicking on img

--- bloom-and-vine/src/Components/gallery-carousel/Components/gallerySlider/components/galleryImage/galleryImage.js
+++ bloom-and-vine/src/Components/gallery-carousel/Components/gallerySlider/components/galleryImage/galleryImage.js
@@ -10,9 +10,9 @@ import './galleryImage.css'
 */
 
 export default function GalleryImage (props) {
-    const { src, altText, imgClass } = props;
+    const { src, altText, imgClass, setIndex, index } = props;
 
     return (
-        <img className={imgClass} src={src} alt={altText} />
+        <img className={imgClass} src={src} alt={altText} onClick={() => setIndex(index)} />
     );
 }
