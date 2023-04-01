795e03af53339c61fa2488c36f9b7d9184d69994
dorothyle <46880658+dorothyle@users.noreply.github.com>
2023-04-01 16:06:08 -0700

    Revert "Revert "clean up index.js""
    This reverts commit 600505bae911cfe2d3d23af5ac3f72f3bcb6a9ee.

--- bloom-and-vine/src/index.js
+++ bloom-and-vine/src/index.js
@@ -4,8 +4,6 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
-import Contact from './Pages/Contact';
-import Navbar from './Components/Navbar';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
