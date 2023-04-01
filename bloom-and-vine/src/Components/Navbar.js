c29c9ee7ca080710dbab1d6114d2c3f5ab34b78b
dorothyle <46880658+dorothyle@users.noreply.github.com>
2023-04-01 16:06:01 -0700

    Revert "Revert "merge navbar, footer, and contact page into main (#19)""
    This reverts commit 61b8d5c5ff2391fa98220c1b599208e0b01112db.

--- bloom-and-vine/src/Components/Navbar.js
+++ bloom-and-vine/src/Components/Navbar.js
@@ -1,10 +1,47 @@
-import './Navbar.css';
+import './Navbar.css'
+import mobile from '../Images/b&v.png'
+import React from 'react';
+
 
 function Navbar() {
   return (
-    <div className="Navbar">
-      
-    </div>
+    <nav className="Navbar">
+      <div className='Logo'>
+        <a href='/Home' className='main-logo'>
+          Bloom & Vine
+        </a>
+      </div>
+      <div className='mobile-logo'>
+        <a href='/Home' className='main-mobile-logo'>
+          <img src={mobile} alt='Bloom & Vine' />
+        </a>
+      </div>
+
+      <ul className='directory'>
+        <li>
+          <a href='/Home'>Home</a>
+        </li>
+        <li>
+          <a href='/Gallery'>Gallery</a>
+        </li>
+        <li>
+          <a href='/Services'>Services</a>
+          </li>
+        <li>
+          <a href='/About'>About</a>
+        </li>
+        <li>
+          <a href='/Contact'>Contact</a>
+        </li>
+      </ul>
+
+      <div className='Contact-Button'>
+          <a href='/Contact'>
+            Contact
+            </a>
+      </div>
+
+    </nav>
   );
 }
 
