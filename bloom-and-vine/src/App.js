9faa1421c0859a7e1b4e5d4b275707b503f3db8d
Ryan <ryyu444@gmail.com>
2023-04-01 00:57:58 -0700

    Changed contact img sizing

--- bloom-and-vine/src/App.js
+++ bloom-and-vine/src/App.js
@@ -7,16 +7,13 @@ import Contact from './Pages/Contact';
 
 function App() {
   return (
-    <div className="App">
-        <Routes>
-            <Route exact path='/Home' element={<Homepage />} />
-            <Route exact path='/Gallery' element={<Gallery />} />
-            <Route exact path='/Services' element={<Services />} />
-            <Route exact path='/About' element={<About />} />
-            <Route exact path='/Contact' element={<Contact />} />
-            <Route path="*" element={<div>error</div>} />
-        </Routes>
-    </div>
+    <Routes>
+        <Route exact path='/Home' element={< Homepage />} />
+        <Route exact path='/Gallery' element={< Gallery />} />
+        <Route exact path='/Services' element={< Services />} />
+        <Route exact path='/About' element={< About />} />
+        <Route exact path='/Contact' element={< Contact />} />
+    </Routes>
   );
 }
 
